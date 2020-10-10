269416ce85d14e6a54ee976204373bceeae044ba
swgil007 <48559894+swgil007@users.noreply.github.com>
2020-10-10 20:36:02 +0900

    Version 0.0.1

---
+++ src/main/java/com/noryangjin/boostcourse/BoostcourseApplication.java
@@ -0,0 +1,13 @@
+package com.noryangjin.boostcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoostcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noryangjin/boostcourse/BoostcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noryangjin.boostcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoostcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
