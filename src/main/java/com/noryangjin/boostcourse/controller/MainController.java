9865c600de15b471cd13c276c161ded5ac199cf9
swgil007 <48559894+swgil007@users.noreply.github.com>
2020-10-24 05:22:59 +0900

    Version 0.5.0
    - front단 작업 초기 구축

--- src/main/java/com/noryangjin/boostcourse/controller/MainController.java
+++ src/main/java/com/noryangjin/boostcourse/controller/MainController.java
@@ -1,5 +1,9 @@
 package com.noryangjin.boostcourse.controller;
 
+import com.noryangjin.boostcourse.domain.Category;
+import com.noryangjin.boostcourse.domain.User;
+import com.noryangjin.boostcourse.dto.CategoryDTO;
+import com.noryangjin.boostcourse.dto.ProductDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +13,15 @@ public class MainController {
 
     @RequestMapping("main")
     public String main(Model model){
-        String message = "테스트임";
-        model.addAttribute("test", message);
-        return "main";
+
+        // 로그인 후 로그인 유저 정보로 대체 예정 테스트용도임
+        ProductDTO.ProductImages productImages = new ProductDTO.ProductImages();
+        productImages.setFileName("testest하나둘셋");
+
+
+
+        model.addAttribute("product", productImages);
+        return "layout/basic";
     }
 
-    @RequestMapping("test")
-    public String test(Model model){
-        String message = "테스트임";
-        model.addAttribute("test", message);
-        return "board/list";
-    }
 }
