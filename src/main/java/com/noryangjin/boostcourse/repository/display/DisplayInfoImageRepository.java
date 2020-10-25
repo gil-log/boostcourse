48e3b0511b3db78e599f47fe2b4e096bc986c1b4
swgil007 <swgil007@naver.com>
2020-10-25 20:18:09 +0900

    Version 0.5.4
    내부 Interface 삽입 중

--- src/main/java/com/noryangjin/boostcourse/repository/display/DisplayInfoImageRepository.java
+++ src/main/java/com/noryangjin/boostcourse/repository/display/DisplayInfoImageRepository.java
@@ -5,6 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DisplayInfoImageRepository extends JpaRepository<DisplayInfoImage, Long>, DisplayInfoImageSupportRepository {
+public interface DisplayInfoImageRepository extends JpaRepository<DisplayInfoImage, Long>, DisplayInfoImageSupportRepositorya {
 
+}
+interface DisplayInfoImageSupportRepositorya {
+
+    // displayInfoId로 displayInfoImage 출력
+    public DisplayInfoImage getDisplayInfoImageByDisplayInfoId(long displayInfoId);
 }
