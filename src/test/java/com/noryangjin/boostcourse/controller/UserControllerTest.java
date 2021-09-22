204b37130dc59f969bf4e841eb387e3991e34d8c
GilLog <swgil007@naver.com>
2021-09-22 20:10:41 +0900

    Update UserControllerTest.java

--- src/test/java/com/noryangjin/boostcourse/controller/UserControllerTest.java
+++ src/test/java/com/noryangjin/boostcourse/controller/UserControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
